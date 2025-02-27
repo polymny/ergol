1c7a8258f9264dfd3883ae090538bd8484283d8c
Thomas Forgione <thomas@forgione.fr>
2025-02-27 13:22:47 +0100

    Fix example

--- examples/one_to_one/src/main.rs
+++ examples/one_to_one/src/main.rs
@@ -46,7 +46,7 @@ pub struct Project {
 #[rustfmt::skip]
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let (mut client, connection) = ergol::connect(
+    let (client, connection) = ergol::connect(
         "host=localhost user=ergol password=ergol",
         NoTls,
     )
@@ -58,75 +58,73 @@ async fn main() -> Result<(), Error> {
         }
     });
 
-    let mut db = client.transaction().await.unwrap();
-
     // Try to delete the database
-    // User::drop_table().execute(&db).await.ok();
-    // Project::drop_table().execute(&db).await.ok();
-    // Gender::drop_type().execute(&db).await.ok();
+    User::drop_table().execute(&client).await.ok();
+    Project::drop_table().execute(&client).await.ok();
+    Gender::drop_type().execute(&client).await.ok();
 
-    // // Create the tables
-    // Gender::create_type().execute(&db).await?;
-    // User::create_table().execute(&db).await?;
-    // Project::create_table().execute(&db).await?;
+    // Create the tables
+    Gender::create_type().execute(&client).await?;
+    User::create_table().execute(&client).await?;
+    Project::create_table().execute(&client).await?;
 
     // Create users
-    User::create("graydon", "graydon@example.com", None, Gender::Male, Data::new()).save(&mut db).await?;
-    // User::create("evan", "evan@example.com", Some(4), Gender::Male, Data::new()).save(&db).await?;
-    // User::create("nico", "nico@example.com", None, Gender::Male, Data::new()).save(&db).await?;
-    // User::create("tforgione", "thomas@forgione.fr", Some(28), Gender::Male, Data::new()).save(&db).await?;
+    User::create("graydon", "graydon@example.com", None, Gender::Male, Data::new()).save(&client).await?;
+    User::create("evan", "evan@example.com", Some(4), Gender::Male, Data::new()).save(&client).await?;
+    User::create("nico", "nico@example.com", None, Gender::Male, Data::new()).save(&client).await?;
+    User::create("tforgione", "thomas@forgione.fr", Some(28), Gender::Male, Data::new()).save(&client).await?;
 
-    // let tforgione = User::get_by_username("tforgione", &db).await?.unwrap();
-    // let graydon = User::get_by_username("graydon", &db).await?.unwrap();
+    let tforgione = User::get_by_username("tforgione", &client).await?.unwrap();
+    let graydon = User::get_by_username("graydon", &client).await?.unwrap();
 
-    // Project::create("My first project", &tforgione).save(&db).await?;
+    Project::create("My first project", &tforgione).save(&client).await?;
 
-    // // Select all users
-    // let mut users = User::select().execute(&db).await?;
+    // Select all users
+    let mut users = User::select().execute(&client).await?;
 
-    // // Update the age
-    // println!("Before update");
-    // for user in &mut users {
-    //     println!("{} {:?} {:?}", user.username, user.age, user.gender);
-    //     if let Some(age) = user.age.as_mut() {
-    //         *age += 1;
-    //     }
-    //     user.gender = Gender::Other;
-    //     user.save(&db).await?;
-    // }
+    // Update the age
+    println!("Before update");
+    for user in &mut users {
+        println!("{} {:?} {:?}", user.username, user.age, user.gender);
+        if let Some(age) = user.age.as_mut() {
+            *age += 1;
+        }
+        user.gender = Gender::Other;
+        user.save(&client).await?;
+    }
 
-    // // Re-select to verify the update
-    // println!("\nAfter update");
-    // let users = User::select().execute(&db).await?;
-    // for user in users {
-    //     println!("{} {:?} {:?}", user.username, user.age, user.gender);
-    // }
+    // Re-select to verify the update
+    println!("\nAfter update");
+    let users = User::select().execute(&client).await?;
+    for user in users {
+        println!("{} {:?} {:?}", user.username, user.age, user.gender);
+    }
 
-    // // Select all projects
-    // println!("\nProjects");
-    // let mut projects = Project::select().execute(&db).await?;
-    // for project in &mut projects {
-    //     let owner = project.owner(&db).await?;
-    //     project.owner = (&graydon).into();
-    //     project.save(&db).await?;
-    //     println!("Project \"{}\" owned by \"{}\"", project.name, owner.username);
-    // }
+    // Select all projects
+    println!("\nProjects");
+    let mut projects = Project::select().execute(&client).await?;
+    for project in &mut projects {
+        let owner = project.owner(&client).await?;
+        project.owner = (&graydon).into();
+        project.save(&client).await?;
+        println!("Project \"{}\" owned by \"{}\"", project.name, owner.username);
+    }
 
-    // // Select all projects
-    // println!("\nProjects");
-    // let projects = Project::select().execute(&db).await?;
-    // for project in projects {
-    //     let owner = project.owner(&db).await?;
-    //     println!("Project \"{}\" owned by \"{}\"", project.name, owner.username);
-    // }
+    // Select all projects
+    println!("\nProjects");
+    let projects = Project::select().execute(&client).await?;
+    for project in projects {
+        let owner = project.owner(&client).await?;
+        println!("Project \"{}\" owned by \"{}\"", project.name, owner.username);
+    }
 
-    // // Exploit the one to one relation
-    // println!("\nProjects");
-    // let project = tforgione.project(&db).await?;
-    // println!("{}'s project is {:?}", tforgione.username, project.map(|x| x.name));
+    // Exploit the one to one relation
+    println!("\nProjects");
+    let project = tforgione.project(&client).await?;
+    println!("{}'s project is {:?}", tforgione.username, project.map(|x| x.name));
 
-    // let project = graydon.project(&db).await?;
-    // println!("{}'s project is {:?}", graydon.username, project.map(|x| x.name));
+    let project = graydon.project(&client).await?;
+    println!("{}'s project is {:?}", graydon.username, project.map(|x| x.name));
 
     Ok(())
 }
